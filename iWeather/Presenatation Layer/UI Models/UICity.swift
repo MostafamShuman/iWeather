c00e3573ddd35004b9a0f8b656d26051c1e703c9
Mostafa Shuman <mostafa.shuman@hotmail.com>
2022-01-30 00:00:04 +0200

    added ui model

---
+++ iWeather/Presenatation Layer/UI Models/UICity.swift
@@ -0,0 +1,14 @@
+//
+//  UICity.swift
+//  iWeather
+//
+//  Created by Mostafa Shuman on 29/01/2022.
+//
+
+import Foundation
+
+struct UICity {
+    let title: String
+    let subtitle: String
+    let image: String
+}
